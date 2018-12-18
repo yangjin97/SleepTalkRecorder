092a52cf17060726d02e2d318c111c12f7bf2686
yangjin97 <yangjin@andrew.cmu.edu>
2018-12-18 18:07:51 -0500

    init

---
+++ SleepTalkRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SleepTalkRecorder
+//
+//  Created by Yang Jin on 12/18/18.
+//  Copyright © 2018 Yang Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
